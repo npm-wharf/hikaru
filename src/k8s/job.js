215ad16bbd2d19f0884589b5bafa5d264fc8fa98
Alexander Early <alex@npmjs.com>
2018-09-06 15:45:57 -0700

    untangle job.js

--- src/k8s/job.js
+++ src/k8s/job.js
@@ -2,6 +2,7 @@ const _ = require('lodash')
 const log = require('bole')('k8s')
 const Promise = require('bluebird')
 const diffs = require('./specDiff')
+const retry = require('../retry')
 
 function base (client, namespace) {
   return client
@@ -17,174 +18,116 @@ function multiple (client, namespace, name) {
   return base(client, namespace).jobs
 }
 
-function checkJob (client, namespace, name, outcome, resolve, reject, wait) {
-  let ms = wait || 500
-  let next = ms + (ms / 2)
-  if (next > 5000) {
-    next = 5000
-  }
-  setTimeout(() => {
+async function checkJob (client, namespace, name, outcome) {
+  await retry(async bail => {
     log.debug(`checking job status '${namespace}.${name}' for '${outcome}'`)
-    single(client, namespace, name).get()
-      .then(
-        result => {
-          try {
-            log.debug(`job '${namespace}.${name}' status - '${JSON.stringify(result.status, null, 2)}'`)
-            const status = result.status.conditions && result.status.conditions.length
-                              ? result.status.conditions[0] : {}
-            if (outcome === 'completion') {
-              if (status.type === 'Complete' && status.status === 'True') {
-                resolve(result)
-              } else if (status.type === 'Failed' && status.status === 'True') {
-                reject(new Error(`Job '${namespace}.${name}' failed to complete with status: '${JSON.stringify(result.status, null, 2)}'`))
-              } else {
-                checkJob(client, namespace, name, outcome, resolve, reject, next)
-              }
-            } else if (outcome === 'updated') {
-              if (status.type === 'Complete' && status.status === 'True') {
-                resolve(result)
-              } else if (status.type === 'Failed' && status.status === 'True') {
-                reject(new Error(`Job '${namespace}.${name}' failed to update with status: '${JSON.stringify(result.status, null, 2)}'`))
-              } else {
-                checkJob(client, namespace, name, outcome, resolve, reject, next)
-              }
-            } else {
-              checkJob(client, namespace, name, outcome, resolve, reject, next)
-            }
-          } catch (e) {
-            log.error(`error checking result '${JSON.stringify(result, null, 2)}':\n\t${e}`)
-          }
-        },
-        () => {
-          if (outcome === 'deletion') {
-            log.debug(`job '${namespace}.${name}' deleted successfully.`)
-            resolve()
-          } else {
-            log.debug(`job '${namespace}.${name}' status check got API error. Checking again in ${next} ms.`)
-            checkJob(client, namespace, name, outcome, resolve, reject, next)
-          }
+    try {
+      var result = await single(client, namespace, name).get()
+    } catch (err) {
+      if (outcome === 'deletion') {
+        log.debug(`job '${namespace}.${name}' deleted successfully.`)
+        return
+      } else {
+        log.debug(`job '${namespace}.${name}' status check got API error. Checking again soon.`)
+        throw new Error('continue')
+      }
+    }
+
+    try {
+      log.debug(`job '${namespace}.${name}' status - '${JSON.stringify(result.status, null, 2)}'`)
+      const status = result.status.conditions && result.status.conditions.length
+                        ? result.status.conditions[0] : {}
+      if (outcome === 'completion') {
+        if (status.type === 'Complete' && status.status === 'True') {
+          return result
+        } else if (status.type === 'Failed' && status.status === 'True') {
+          bail(new Error(`Job '${namespace}.${name}' failed to complete with status: '${JSON.stringify(result.status, null, 2)}'`))
         }
-      )
-  }, ms)
+      } else if (outcome === 'updated') {
+        if (status.type === 'Complete' && status.status === 'True') {
+          return result
+        } else if (status.type === 'Failed' && status.status === 'True') {
+          bail(new Error(`Job '${namespace}.${name}' failed to update with status: '${JSON.stringify(result.status, null, 2)}'`))
+        }
+      }
+    } catch (e) {
+      log.error(`error checking result '${JSON.stringify(result, null, 2)}':\n\t${e}`)
+    }
+    throw new Error('continue')
+  })
 }
 
-function createJob (client, deletes, jobSpec) {
+async function createJob (client, deletes, jobSpec) {
   const namespace = jobSpec.metadata.namespace || 'default'
   const name = jobSpec.metadata.name
-  let create = (resolve, reject) =>
-    multiple(client, namespace).create(jobSpec)
-    .then(
-      result => {
-        checkJob(client, namespace, name, 'completion', resolve, reject)
-      },
-      err => {
-        reject(new Error(` Job '${namespace}.${name}' failed to create:\n\t${err.message}`))
+  let create = async () => {
+    await multiple(client, namespace).create(jobSpec)
+      .catch(err => {
+        throw new Error(` Job '${namespace}.${name}' failed to create:\n\t${err.message}`)
+      })
+    await checkJob(client, namespace, name, 'completion')
+  }
+  try {
+    var loaded = await single(client, namespace, name).get()
+  } catch (err) {
+    await create()
+  }
+  const diff = diffs.simple(loaded, jobSpec)
+  if (!_.isEmpty(diff)) {
+    if (diffs.canPatch(diff, 'job') || diffs.isBackoffOnly(diff, jobSpec)) {
+      if (client.saveDiffs) {
+        diffs.save(loaded, jobSpec, diff)
       }
-    )
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).get()
-      .then(
-        loaded => {
-          const diff = diffs.simple(loaded, jobSpec)
-          if (_.isEmpty(diff)) {
-            resolve()
-          } else {
-            if (diffs.canPatch(diff, 'job') || diffs.isBackoffOnly(diff, jobSpec)) {
-              if (client.saveDiffs) {
-                diffs.save(loaded, jobSpec, diff)
-              }
-              updateJob(client, namespace, name, diff)
-                .then(
-                  resolve,
-                  reject
-                )
-            } else if (diffs.canReplace(diff, 'job')) {
-              replaceJob(client, namespace, name, jobSpec)
-                .then(
-                  resolve,
-                  reject
-                )
-            } else {
-              deleteJob(client, namespace, name)
-                .then(
-                  create.bind(null, resolve, reject),
-                  reject
-                )
-            }
-          }
-        },
-        create.bind(null, resolve, reject)
-      )
-  })
+      await updateJob(client, namespace, name, diff)
+    } else if (diffs.canReplace(diff, 'job')) {
+      await replaceJob(client, namespace, name, jobSpec)
+    } else {
+      await deleteJob(client, namespace, name)
+      await create()
+    }
+  }
 }
 
-function deleteJob (client, namespace, name) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).get()
-      .then(
-        () => {
-          single(client, namespace, name).delete()
-            .then(
-              result => {
-                checkJob(client, namespace, name, 'deletion', resolve)
-              },
-              err => {
-                reject(new Error(`Job '${namespace}.${name}' could not be deleted:\n\t${err.message}`))
-              }
-            )
-        },
-        () => { resolve() }
-      )
-  })
+async function deleteJob (client, namespace, name) {
+  try {
+    await single(client, namespace, name).get()
+    await single(client, namespace, name).delete()
+      .catch(err => {
+        throw new Error(`Job '${namespace}.${name}' could not be deleted:\n\t${err.message}`)
+      })
+    await checkJob(client, namespace, name, 'deletion')
+  } catch (err) {}
 }
 
 function listJobs (client, namespace) {
   return multiple(client, namespace).list()
 }
 
-function replaceJob (client, namespace, name, spec) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).update(spec)
-      .then(
-        result => {
-          checkJob(client, namespace, name, 'updated', resolve)
-        },
-        err => {
-          reject(new Error(`Job '${namespace}.${name}' failed to replace:\n\t${err.message}`))
-        }
-      )
-  })
+async function replaceJob (client, namespace, name, spec) {
+  await single(client, namespace, name).update(spec)
+    .catch(err => {
+      throw new Error(`Job '${namespace}.${name}' failed to replace:\n\t${err.message}`)
+    })
+  await checkJob(client, namespace, name, 'updated')
 }
 
-function runJob (client, deletes, namespace, name, spec) {
-  const steps = [
-    () => deleteJob(client, namespace, name),
-    () => createJob(client, deletes, spec)
-  ]
-  return Promise.mapSeries(steps, s => s())
-    .then(
-      () => {
-        log.info(`job '${name}' in namespace '${namespace}' completed successfully`)
-      },
-      err => {
-        log.error(`running job '${name}' in namespace '${namespace}' failed with: ${err.stack}`)
-        throw new Error(`running job '${name}' in namespace '${namespace}' failed`)
-      }
-    )
+async function runJob (client, deletes, namespace, name, spec) {
+  try {
+    await deleteJob(client, namespace, name)
+    await createJob(client, deletes, spec)
+  } catch (err) {
+    log.error(`running job '${name}' in namespace '${namespace}' failed with: ${err.stack}`)
+    throw new Error(`running job '${name}' in namespace '${namespace}' failed`)
+  }
+  log.info(`job '${name}' in namespace '${namespace}' completed successfully`)
 }
 
-function updateJob (client, namespace, name, diff) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).patch(diff)
-      .then(
-        result => {
-          checkJob(client, namespace, name, 'updated', resolve)
-        },
-        err => {
-          reject(new Error(`Job '${namespace}.${name}' failed to update:\n\t${err.message}`))
-        }
-      )
-  })
+async function updateJob (client, namespace, name, diff) {
+  await single(client, namespace, name).patch(diff)
+    .catch(err => {
+      throw new Error(`Job '${namespace}.${name}' failed to update:\n\t${err.message}`)
+    })
+  await checkJob(client, namespace, name, 'updated')
 }
 
 module.exports = function (client, deletes) {
