92ac64a8b26e5dbcb48b239c4d2879ae113738f9
Alexander Early <alex@npmjs.com>
2018-09-06 13:38:24 -0700

    untangle cronjob.js

--- src/k8s/cronJob.js
+++ src/k8s/cronJob.js
@@ -1,7 +1,8 @@
 const _ = require('lodash')
 const log = require('bole')('k8s')
-const Promise = require('bluebird')
 const diffs = require('./specDiff')
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const GROUPS = {
   '1.7': 'batch/v2alpha1',
@@ -26,163 +27,122 @@ function multiple (client, namespace) {
   return base(client, namespace).cronjobs
 }
 
-function checkCronJob (client, namespace, name, outcome, resolve, reject, wait) {
-  let ms = wait || 500
-  let next = ms + (ms / 2)
-  if (next > 5000) {
-    next = 5000
-  }
-  setTimeout(() => {
+async function checkCronJob (client, namespace, name, outcome, wait = 500) {
+  let tries = 0
+  do {
+    wait *= 1.5
+    if (wait > 5000) {
+      wait = 5000
+    }
+    tries++
+    await delay(wait)
     log.debug(`checking cron job status '${namespace}.${name}' for '${outcome}'`)
-    single(client, namespace, name).get()
-      .then(
-        result => {
-          try {
-            log.debug(`cron job '${namespace}.${name}' status - '${JSON.stringify(result.status, null, 2)}'`)
-            const status = result.status.conditions && result.status.conditions.length
-                              ? result.status.conditions[0] : {}
-            if (outcome === 'completion') {
-              if (result.status === undefined || Object.keys(result.status).length === 0) {
-                log.info(`cron job appears to have created successfully but did not run (according to schedule)`)
-                resolve(result)
-              } else if (status.type === 'Complete' && status.status === 'True') {
-                resolve(result)
-              } else if (status.type === 'Failed' && status.status === 'True') {
-                reject(new Error(`CronJob '${namespace}.${name}' failed to complete with status: '${JSON.stringify(result.status, null, 2)}'`))
-              } else {
-                checkCronJob(client, namespace, name, outcome, resolve, reject, next)
-              }
-            } else if (outcome === 'updated') {
-              if (result.status === undefined || Object.keys(result.status).length === 0) {
-                log.info(`cron job appears to have updated successfully but did not run (according to schedule)`)
-                resolve(result)
-              } else if (status.type === 'Complete' && status.status === 'True') {
-                resolve(result)
-              } else if (status.type === 'Failed' && status.status === 'True') {
-                reject(new Error(`CronJob '${namespace}.${name}' failed to update with status: '${JSON.stringify(result.status, null, 2)}'`))
-              } else {
-                checkCronJob(client, namespace, name, outcome, resolve, reject, next)
-              }
-            } else {
-              checkCronJob(client, namespace, name, outcome, resolve, reject, next)
-            }
-          } catch (e) {
-            log.error(`error checking result '${JSON.stringify(result, null, 2)}':\n\t${e}`)
-          }
-        },
-        () => {
-          if (outcome === 'deletion') {
-            log.debug(`cron job '${namespace}.${name}' deleted successfully.`)
-            resolve()
-          } else {
-            log.debug(`cron job '${namespace}.${name}' status check got API error. Checking again in ${next} ms.`)
-            checkCronJob(client, namespace, name, outcome, resolve, reject, next)
-          }
+    try {
+      var result = await single(client, namespace, name).get()
+    } catch (err) {
+      if (outcome === 'deletion') {
+        log.debug(`cron job '${namespace}.${name}' deleted successfully.`)
+        return result
+      } else {
+        log.debug(`cron job '${namespace}.${name}' status check got API error. Checking again in ${wait} ms.`)
+        continue
+      }
+    }
+
+    try {
+      log.debug(`cron job '${namespace}.${name}' status - '${JSON.stringify(result.status, null, 2)}'`)
+      const status = result.status.conditions && result.status.conditions.length
+                        ? result.status.conditions[0] : {}
+      if (outcome === 'completion') {
+        if (result.status === undefined || Object.keys(result.status).length === 0) {
+          log.info(`cron job appears to have created successfully but did not run (according to schedule)`)
+          return result
+        } else if (status.type === 'Complete' && status.status === 'True') {
+          return result
+        } else if (status.type === 'Failed' && status.status === 'True') {
+          throw new Error(`CronJob '${namespace}.${name}' failed to complete with status: '${JSON.stringify(result.status, null, 2)}'`)
         }
-      )
-  }, ms)
+      } else if (outcome === 'updated') {
+        if (result.status === undefined || Object.keys(result.status).length === 0) {
+          log.info(`cron job appears to have updated successfully but did not run (according to schedule)`)
+          return result
+        } else if (status.type === 'Complete' && status.status === 'True') {
+          return result
+        } else if (status.type === 'Failed' && status.status === 'True') {
+          throw new Error(`CronJob '${namespace}.${name}' failed to update with status: '${JSON.stringify(result.status, null, 2)}'`)
+        }
+      }
+    } catch (e) {
+      log.error(`error checking result '${JSON.stringify(result, null, 2)}':\n\t${e}`)
+    }
+  } while (tries < 20)
 }
 
-function createCronJob (client, deletes, jobSpec) {
+async function createCronJob (client, deletes, jobSpec) {
   const namespace = jobSpec.metadata.namespace || 'default'
   const name = jobSpec.metadata.name
-  let create = (resolve, reject) =>
-    multiple(client, namespace).create(jobSpec)
-    .then(
-      result => {
-        checkCronJob(client, namespace, name, 'completion', resolve, reject)
-      },
-      err => {
-        reject(new Error(`Cron Job '${namespace}.${name}' failed to create:\n\t${err.message}`))
+
+  const create = async () => {
+    await multiple(client, namespace).create(jobSpec)
+      .catch(err => {
+        throw new Error(`Cron Job '${namespace}.${name}' failed to create:\n\t${err.message}`)
+      })
+    await checkCronJob(client, namespace, name, 'completion')
+  }
+
+  try {
+    var loaded = await single(client, namespace, name).get()
+  } catch (err) {
+    return create()
+  }
+
+  const diff = diffs.simple(loaded, jobSpec)
+  if (!_.isEmpty(diff)) {
+    if (diffs.canPatch(diff, 'job') || diffs.isBackoffOnly(diff, jobSpec)) {
+      if (client.saveDiffs) {
+        diffs.save(loaded, jobSpec, diff)
       }
-    )
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).get()
-      .then(
-        loaded => {
-          const diff = diffs.simple(loaded, jobSpec)
-          if (_.isEmpty(diff)) {
-            resolve()
-          } else {
-            if (diffs.canPatch(diff, 'job') || diffs.isBackoffOnly(diff, jobSpec)) {
-              if (client.saveDiffs) {
-                diffs.save(loaded, jobSpec, diff)
-              }
-              updateCronJob(client, namespace, name, diff)
-                .then(
-                  resolve,
-                  reject
-                )
-            } else if (diffs.canReplace(diff, 'job')) {
-              replaceCronJob(client, namespace, name, jobSpec)
-                .then(
-                  resolve,
-                  reject
-                )
-            } else {
-              deleteCronJob(client, namespace, name)
-                .then(
-                  create.bind(null, resolve, reject),
-                  reject
-                )
-            }
-          }
-        },
-        create.bind(null, resolve, reject)
-      )
-  })
+      return updateCronJob(client, namespace, name, diff)
+    } else if (diffs.canReplace(diff, 'job')) {
+      return replaceCronJob(client, namespace, name, jobSpec)
+    } else {
+      await deleteCronJob(client, namespace, name)
+      return create()
+    }
+  }
 }
 
-function deleteCronJob (client, namespace, name) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).get()
-      .then(
-        () => {
-          single(client, namespace, name).delete()
-            .then(
-              result => {
-                checkCronJob(client, namespace, name, 'deletion', resolve)
-              },
-              err => {
-                reject(new Error(`Job '${namespace}.${name}' could not be deleted:\n\t${err.message}`))
-              }
-            )
-        },
-        () => { resolve() }
-      )
-  })
+async function deleteCronJob (client, namespace, name) {
+  try {
+    await single(client, namespace, name).get()
+  } catch (err) {
+    return
+  }
+  single(client, namespace, name).delete()
+    .catch(err => {
+      throw new Error(`Job '${namespace}.${name}' could not be deleted:\n\t${err.message}`)
+    })
+  await checkCronJob(client, namespace, name, 'deletion')
 }
 
-function listCronJobs (client, namespace) {
+async function listCronJobs (client, namespace) {
   return multiple(client, namespace).list()
 }
 
-function replaceCronJob (client, namespace, name, spec) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).update(spec)
-      .then(
-        result => {
-          checkCronJob(client, namespace, name, 'updated', resolve)
-        },
-        err => {
-          reject(new Error(`CronJob '${namespace}.${name}' failed to replace:\n\t${err.message}`))
-        }
-      )
-  })
+async function replaceCronJob (client, namespace, name, spec) {
+  await single(client, namespace, name).update(spec)
+    .catch(err => {
+      throw new Error(`CronJob '${namespace}.${name}' failed to replace:\n\t${err.message}`)
+    })
+  await checkCronJob(client, namespace, name, 'updated')
 }
 
-function updateCronJob (client, namespace, name, diff) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).patch(diff)
-      .then(
-        result => {
-          checkCronJob(client, namespace, name, 'updated', resolve)
-        },
-        err => {
-          reject(new Error(`CronJob '${namespace}.${name}' failed to update:\n\t${err.message}`))
-        }
-      )
-  })
+async function updateCronJob (client, namespace, name, diff) {
+  await single(client, namespace, name).patch(diff)
+    .catch(err => {
+      throw new Error(`CronJob '${namespace}.${name}' failed to update:\n\t${err.message}`)
+    })
+  await checkCronJob(client, namespace, name, 'updated')
 }
 
 module.exports = function (client, deletes) {
