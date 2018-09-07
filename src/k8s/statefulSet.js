ee1f62b29ec191e2a9c0ef809b808c8c99866e9c
Alexander Early <alex@npmjs.com>
2018-09-06 17:13:17 -0700

    refactor statefulSet

--- src/k8s/statefulSet.js
+++ src/k8s/statefulSet.js
@@ -4,6 +4,7 @@ const Promise = require('bluebird')
 const core = require('./core')
 const diffs = require('./specDiff')
 const parse = require('../imageParser').parse
+const retry = require('../retry')
 
 const GROUPS = {
   '1.4': 'apps/v1beta1',
@@ -31,183 +32,131 @@ function multiple (client, namespace, name) {
   return base(client, namespace).statefulsets
 }
 
-function checkStatefulSet (client, namespace, name, outcome, resolve, wait) {
-  let ms = wait || 500
-  let next = ms + (ms / 2)
-  if (next > 5000) {
-    next = 5000
-  }
-  log.debug(`checking statefulSet status '${namespace}.${name}' for '${outcome}'`)
-  setTimeout(() => {
-    single(client, namespace, name).get()
-      .then(
-        result => {
-          log.debug(`statefulSet '${namespace}.${name}' status - '${JSON.stringify(result.status, null, 2)}'`)
-          if (outcome === 'creation' && result.status.readyReplicas > 0) {
-            resolve(result)
-          } else if (outcome === 'updated' && result.status.readyReplicas > 0) {
-            resolve(result)
-          } else if (outcome === 'deletion' && result.status.phase !== 'Terminating') {
-            resolve(result)
-          } else {
-            checkStatefulSet(client, namespace, name, outcome, resolve, next)
-          }
-        },
-        () => {
-          if (outcome === 'deletion') {
-            log.debug(`statefulSet '${namespace}.${name}' deleted successfully.`)
-            resolve()
-          } else {
-            log.debug(`statefulSet '${namespace}.${name}' status check got API error. Checking again in ${next} ms.`)
-            checkStatefulSet(client, namespace, name, outcome, resolve, next)
-          }
-        }
-      )
-  }, ms)
+async function checkStatefulSet (client, namespace, name, outcome) {
+  return retry(async () => {
+    log.debug(`checking statefulSet status '${namespace}.${name}' for '${outcome}'`)
+    try {
+      var result = await single(client, namespace, name).get()
+    } catch (err) {
+      if (outcome === 'deletion') {
+        log.debug(`statefulSet '${namespace}.${name}' deleted successfully.`)
+        return
+      } else {
+        log.debug(`statefulSet '${namespace}.${name}' status check got API error. Checking again soon.`)
+        throw new Error('continue')
+      }
+    }
+
+    log.debug(`statefulSet '${namespace}.${name}' status - '${JSON.stringify(result.status, null, 2)}'`)
+    if (outcome === 'creation' && result.status.readyReplicas > 0) {
+      return result
+    } else if (outcome === 'updated' && result.status.readyReplicas > 0) {
+      return result
+    } else if (outcome === 'deletion' && result.status.phase !== 'Terminating') {
+      return result
+    }
+
+    throw new Error('continue')
+  })
 }
 
-function createStatefulSet (client, deletes, statefulSet) {
+async function createStatefulSet (client, deletes, statefulSet) {
   const namespace = statefulSet.metadata.namespace || 'default'
   const name = statefulSet.metadata.name
-  let create = (resolve, reject) =>
-    multiple(client, namespace).create(statefulSet)
-    .then(
-      result => {
-        checkStatefulSet(client, namespace, name, 'creation', resolve)
-      },
-      err => {
-        reject(new Error(`StatefulSet '${namespace}.${name}' failed to create:\n\t${err.message}`))
+  let create = async () => {
+    await multiple(client, namespace).create(statefulSet)
+      .catch(err => {
+        throw new Error(`StatefulSet '${namespace}.${name}' failed to create:\n\t${err.message}`)
+      })
+
+    await checkStatefulSet(client, namespace, name, 'creation')
+  }
+
+  try {
+    var loaded = await single(client, namespace, name).get()
+  } catch (e) {
+    return create()
+  }
+  const diff = diffs.simple(loaded, statefulSet)
+  if (!_.isEmpty(diff)) {
+    if (diffs.canPatch(diff)) {
+      if (client.saveDiffs) {
+        diffs.save(loaded, statefulSet, diff)
       }
-    )
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).get()
-      .then(
-        loaded => {
-          const diff = diffs.simple(loaded, statefulSet)
-          if (_.isEmpty(diff)) {
-            resolve()
-          } else {
-            if (diffs.canPatch(diff)) {
-              if (client.saveDiffs) {
-                diffs.save(loaded, statefulSet, diff)
-              }
-              updateStatefulSet(client, namespace, name, diff)
-                .then(
-                  resolve,
-                  reject
-                )
-            } else if (diffs.canReplace(diff)) {
-              replaceStatefulSet(client, namespace, name, statefulSet)
-                .then(
-                  resolve,
-                  reject
-                )
-            } else {
-              deleteStatefulSet(client, namespace, name)
-                .then(
-                  create.bind(null, resolve, reject),
-                  reject
-                )
-            }
-          }
-        },
-        create.bind(null, resolve, reject)
-      )
-  })
+      await updateStatefulSet(client, namespace, name, diff)
+    } else if (diffs.canReplace(diff)) {
+      await replaceStatefulSet(client, namespace, name, statefulSet)
+    } else {
+      await deleteStatefulSet(client, namespace, name)
+      await create()
+    }
+  }
 }
 
-function deleteStatefulSet (client, namespace, name) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).get()
-      .then(
-        () => {
-          single(client, namespace, name).delete()
-            .then(
-              result => {
-                checkStatefulSet(client, namespace, name, 'deletion', resolve)
-              },
-              err => {
-                reject(new Error(`StatefulSet '${namespace}.${name}' could not be deleted:\n\t${err.message}`))
-              }
-            )
-        },
-        () => { resolve() }
-      )
-  })
+async function deleteStatefulSet (client, namespace, name) {
+  try {
+    await single(client, namespace, name).get()
+  } catch (e) {
+    return
+  }
+  await single(client, namespace, name).delete()
+    .catch(err => {
+      throw new Error(`StatefulSet '${namespace}.${name}' could not be deleted:\n\t${err.message}`)
+    })
+  await checkStatefulSet(client, namespace, name, 'deletion')
 }
 
-function getStatefulSetsByNamespace (client, namespace, baseImage) {
-  return listStatefulSets(client, namespace)
-      .then(
-        list => {
-          let statefulSets = _.reduce(list.items, (acc, spec) => {
-            const containers = core.getContainersFromSpec(spec, baseImage)
-            containers.forEach(container => {
-              const metadata = parse(container.image)
-              acc.push({
-                namespace: namespace,
-                type: 'StatefulSet',
-                service: spec.metadata.name,
-                image: container.image,
-                container: container.name,
-                metadata: metadata,
-                labels: spec.spec.template.metadata
-                  ? spec.spec.template.metadata.labels
-                  : metadata.labels || {}
-              })
-            })
-            return acc
-          }, [])
-          return { namespace, statefulSets }
-        }
-      )
+async function getStatefulSetsByNamespace (client, namespace, baseImage) {
+  const list = await listStatefulSets(client, namespace)
+  let statefulSets = _.reduce(list.items, (acc, spec) => {
+    const containers = core.getContainersFromSpec(spec, baseImage)
+    containers.forEach(container => {
+      const metadata = parse(container.image)
+      acc.push({
+        namespace: namespace,
+        type: 'StatefulSet',
+        service: spec.metadata.name,
+        image: container.image,
+        container: container.name,
+        metadata: metadata,
+        labels: spec.spec.template.metadata
+          ? spec.spec.template.metadata.labels
+          : metadata.labels || {}
+      })
+    })
+    return acc
+  }, [])
+
+  return { namespace, statefulSets }
 }
 
-function listStatefulSets (client, namespace) {
+async function listStatefulSets (client, namespace) {
   return multiple(client, namespace).list()
 }
 
-function replaceStatefulSet (client, namespace, name, spec) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).update(spec)
-      .then(
-        result => {
-          checkStatefulSet(client, namespace, name, 'updated', resolve)
-        },
-        err => {
-          reject(new Error(`StatefulSet '${namespace}.${name}' failed to replace:\n\t${err.message}`))
-        }
-      )
-  })
+async function replaceStatefulSet (client, namespace, name, spec) {
+  await single(client, namespace, name).update(spec)
+    .catch(err => {
+      throw new Error(`StatefulSet '${namespace}.${name}' failed to replace:\n\t${err.message}`)
+    })
+  await checkStatefulSet(client, namespace, name, 'updated')
 }
 
-function updateStatefulSet (client, namespace, name, patch) {
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).patch(patch)
-      .then(
-        result => {
-          checkStatefulSet(client, namespace, name, 'updated', resolve)
-        },
-        err => {
-          reject(new Error(`StatefulSet '${namespace}.${name}' failed to update:\n\t${err.message}`))
-        }
-      )
-  })
+async function updateStatefulSet (client, namespace, name, patch) {
+  await single(client, namespace, name).patch(patch)
+    .catch(err => {
+      throw new Error(`StatefulSet '${namespace}.${name}' failed to update:\n\t${err.message}`)
+    })
+  await checkStatefulSet(client, namespace, name, 'updated')
 }
 
-function upgradeStatefulSet (client, namespace, name, image, container) {
+async function upgradeStatefulSet (client, namespace, name, image, container) {
   const patch = diffs.getImagePatch(container || name, image)
-  return new Promise((resolve, reject) => {
-    single(client, namespace, name).patch(patch)
-      .then(
-        result => {
-          checkStatefulSet(client, namespace, name, 'update', resolve)
-        },
-        err => {
-          reject(new Error(`StatefulSet '${namespace}.${name}' failed to upgrade:\n\t${err.message}`))
-        }
-      )
-  })
+  await single(client, namespace, name).patch(patch)
+    .catch(err => {
+      throw new Error(`StatefulSet '${namespace}.${name}' failed to upgrade:\n\t${err.message}`)
+    })
+  await checkStatefulSet(client, namespace, name, 'update')
 }
 
 module.exports = function (client, deletes) {
