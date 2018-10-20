44125d677121b8333d1ff2f09c1a6a97a2ad9cf7
Alex Robson <asrobson@gmail.com>
2018-10-19 20:29:44 -0500

    Add support for versions 1.9-1.12 of Kubernetes (#8)
    * feat: add support for secrets, add support for versions through 1.12, fix issues with stateful and daemonset creation.
    
    * fix: add latest version of mcgonagall

--- src/retry.js
+++ src/retry.js
@@ -2,7 +2,11 @@
 
 const asyncRetry = require('async-retry')
 
-module.exports = async function retry (asyncFn, wait = 500, retries = 20) {
+// bumping the retries to 65 to prevent failures from waiting
+// 65 retries is roughly 306 seconds, or 5 minutes.
+// in extreme cases, I have seen containers take this long to start
+// mostly due to download times to clusters in APAC zones
+module.exports = async function retry (asyncFn, wait = 500, retries = 65) {
   return asyncRetry(asyncFn, {
     retries,
     factor: 1.5,
