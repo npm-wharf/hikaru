23afe8ac658e422499f325afa5c808bdb801638a
Nathan LaFreniere <quitlahok@gmail.com>
2019-06-28 13:04:46 -0700

    chore: remove unused server pieces

--- src/config.js
+++ src/config.js
@@ -11,9 +11,6 @@ const KEY = 'K8S_KEY'
 const USERNAME = 'K8S_USERNAME'
 const PASSWORD = 'K8S_PASSWORD'
 const LOG = 'K8S_LOG_LEVEL'
-const AUTH = 'API_TOKEN'
-const LOCAL_KEY = 'LOCAL_PRIVATE_KEY'
-const PUBLIC_KEY = 'REMOTE_PUBLIC_KEY'
 
 function readIfFile (value) {
   if (!value) return
@@ -42,13 +39,6 @@ module.exports = function () {
     ca: readIfFile(process.env[ CA ]),
     cert: readIfFile(process.env[ CERT ]),
     key: readIfFile(process.env[ KEY ]),
-    publicKey: process.env[ PUBLIC_KEY ],
-    localKey: process.env[ LOCAL_KEY ],
-    apiToken: process.env[ AUTH ],
-    transports: [ 'deftly-express' ],
-    resources: [ './src/resources/*.js' ],
-    plugins: [ './src/plugins/*.js' ],
-    middleware: [ './src/middleware/*.js' ],
     logging: {
       level: process.env[LOG] || 'debug'
     }
